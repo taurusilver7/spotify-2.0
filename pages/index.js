7c28bddb533f27aee9bedd20c3d21a7282ab100a
taurusilver7 <prasad20697@gmail.com>
2021-11-30 16:51:33 +0530

    build marking

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Spotify 2.0 Build initiative starts now. this is dope.</h1>
+      <h1>Spotify 2.0 Build</h1>
 
       <main>
         {/* Sidebar */}
         {/* Center */}
       </main>
-      <div> {/*Player*/} </div>
+      <div>{/*Player*/}</div>
     </div>
   );
 }
