401e56971b5d76e7da57ddec27df05c7c9537135
taurusilver7 <prasad20697@gmail.com>
2021-12-02 17:52:59 +0530

    refresh acces token function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,24 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { LOGIN_URL } from "../../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedAccessToken } =
+      await spotifyApi.refreshAccessToken();
+    console.log("REFRESHED TOKEN IS", refreshedToken);
+  } catch {
+    console.error(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,15 +31,32 @@ export default NextAuth({
     // ...add more providers here
   ],
   secret: process.env.JWT_SECRET,
-//   custom login page.
+  //   custom login page.
   pages: {
-      signIn: '/login',
+    signIn: "/login",
   },
   callbacks: {
-      async jwt({token, account, user}){
-
-        // initial sign in
-        
+    async jwt({ token, account, user }) {
+      // initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refeshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000, // handling expiry time in ms.
+        };
       }
-  }
+
+      //   Return previous token if the access token has not expired yet.
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("EXISTING ACCESS TOKEN IS VALID");
+        return token;
+      }
+      // Access token expires, refresh it.
+      console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
+
+      return await refreshAccessToken(token);
+    },
+  },
 });
