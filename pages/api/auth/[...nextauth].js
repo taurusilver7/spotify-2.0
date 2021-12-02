e88c22495fa388e4fa104d275310b643a0bebd12
taurusilver7 <prasad20697@gmail.com>
2021-12-02 13:26:41 +0530

    API added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import SpotifyProvider from "next-auth/providers/spotify";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_ID,
+      clientSecret: process.env.SPOTIFY_SECRET,
     }),
     // ...add more providers here
   ],
