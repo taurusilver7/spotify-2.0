d4e06823897a4f176bbceaaa878d0c94814f1d61
taurusilver7 <prasad20697@gmail.com>
2021-12-02 12:47:12 +0530

    handler for auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,13 @@
-export const [...nextauth] = () => {
-  return (
-    <div>
-      <p>next auth function</p>
-    </div>
-  );
-};
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
