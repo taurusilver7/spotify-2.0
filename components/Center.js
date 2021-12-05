75c46400580bbad05c11dabf5920ed86e60310a0
taurusilver7 <prasad20697@gmail.com>
2021-12-05 22:34:23 +0530

    image added

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,15 @@
+import {useSession} from 'next-auth/react';
+
 const Center = () => {
+  const {data: session} = useSession();
   return (
     <div className="flex flex-grow text-white">
       <h1>Center logic for spotify</h1>
+      <header>
+        <div>
+          <img className='rounded-full w-10 h-10' src={session?.user.image} alt="" />
+        </div>
+      </header>
     </div>
   );
 };
