903fa89a6f188cb63d0fd2ab465b2981c0d15047
taurusilver7 <prasad20697@gmail.com>
2021-12-06 15:40:06 +0530

    section styling

--- components/Center.js
+++ components/Center.js
@@ -7,7 +7,7 @@ const Center = () => {
     <div className="flex flex-grow text-white">
       <h1>Center logic for spotify</h1>
       <header>
-        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full">
+        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
@@ -17,6 +17,12 @@ const Center = () => {
           <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
+
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}
+      >
+        <img src="" alt="" />
+      </section>
     </div>
   );
 };
