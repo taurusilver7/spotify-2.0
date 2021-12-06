d6bc10f22c0671ff679e51bae36e8864ccdff422
taurusilver7 <prasad20697@gmail.com>
2021-12-06 15:41:19 +0530

    header styling.

--- components/Center.js
+++ components/Center.js
@@ -4,9 +4,9 @@ import { ChevronDownIcon } from "@heroicons/react/outline";
 const Center = () => {
   const { data: session } = useSession();
   return (
-    <div className="flex flex-grow text-white">
+    <div className="flex flex-grow">
       <h1>Center logic for spotify</h1>
-      <header>
+      <header className="absolute top-5 right-8">
         <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
           <img
             className="rounded-full w-10 h-10"
@@ -21,7 +21,7 @@ const Center = () => {
       <section
         className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}
       >
-        <img src="" alt="" />
+        {/* <img src="" alt="" /> */}
       </section>
     </div>
   );
