5db3030482c251d545e0e070841428acbb6f5cf4
taurusilver7 <prasad20697@gmail.com>
2021-12-06 15:36:15 +0530

    icon added for playlist

--- components/Center.js
+++ components/Center.js
@@ -1,13 +1,20 @@
-import {useSession} from 'next-auth/react';
+import { useSession } from "next-auth/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
 
 const Center = () => {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   return (
     <div className="flex flex-grow text-white">
       <h1>Center logic for spotify</h1>
       <header>
         <div>
-          <img className='rounded-full w-10 h-10' src={session?.user.image} alt="" />
+          <img
+            className="rounded-full w-10 h-10"
+            src={session?.user.image}
+            alt=""
+          />
+          <h2>{session?.user.name}</h2>
+          <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
     </div>
