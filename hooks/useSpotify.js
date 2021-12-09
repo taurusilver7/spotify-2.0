81d8b1291f2324edebca16aab9f1695f59dec24f
taurusilver7 <prasad20697@gmail.com>
2021-12-09 23:50:59 +0530

    credentials forced to spotifyApi created

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,9 +1,18 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
+import spotifyApi from "../lib/spotify";
 
 const useSpotify = () => {
   const { data: session, status } = useSession();
-  useEffect(() => {}, [session]);
+  useEffect(() => {
+    if (session) {
+      // force sign-in the user if refresh token broke. If refresh access token attempt fails, direct the user to login page.
+      if (session.error === "RefreshAccessTokenError") {
+        signIn();
+      }
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, [session]);
   return null;
 };
 
