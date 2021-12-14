fc3e65ec8f1ff098cd3db9690badf3c004518647
taurusilver7 <prasad20697@gmail.com>
2021-12-14 23:55:31 +0530

    custom hook added in sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,6 +11,7 @@ import { useState } from "react";
 import spotifyApi from "../lib/spotify";
 
 const Sidebar = () => {
+  const spotiftyApi = useSpotify();
   const { data: session, status } = useSession();
   const [playlists, setPlaylists] = useState([]);
 
