c2f8baf1db2ce001b8db8552eaa11ce22a5d52a9
taurusilver7 <prasad20697@gmail.com>
2021-12-20 14:37:39 +0530

    p-tags custom

--- components/Song.js
+++ components/Song.js
@@ -4,7 +4,7 @@ import { msToMinsAndSecs } from "../lib/time";
 const Song = ({ track, order }) => {
   const spotifyApi = useSpotify();
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg">
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
@@ -13,13 +13,15 @@ const Song = ({ track, order }) => {
           alt="album-image"
         />
         <div>
-          <p>{track.track.name}</p>
-          <p>{track.track.artists[0].name}</p>
+          <p className="w-36 lg:w-64 text-white truncate">{track.track.name}</p>
+          <p className="w-40">{track.track.artists[0].name}</p>
         </div>
       </div>
 
       <div className="flex items-center justify-between ml-auto md:ml-0">
-        <p className='hidden md:inline'>{track.track.album.name}</p>
+        <p className="w-40 truncate hidden md:inline">
+          {track.track.album.name}
+        </p>
         <p>{msToMinsAndSecs(track.track.duration_ms)}</p>
       </div>
     </div>
