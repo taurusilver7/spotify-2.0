5e039808847ec0f53a00379da542fb618ebb1ce9
taurusilver7 <prasad20697@gmail.com>
2021-12-22 22:36:14 +0530

    song atom created

--- atoms/songAtom.js
+++ atoms/songAtom.js
@@ -1,3 +1,11 @@
 import { atom } from "recoil";
 
-const 
+export const currentTrackIdState = atom({
+  key: "currentTrackIdState", //a unique ID w.r.t other atoms/selectors
+  default: null, // default value (initial value)
+});
+
+export const isPlayingState = atom({
+  key: "isPlayingState",
+  default: false,
+});
