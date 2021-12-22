528a8a519fb96d73b98702c2498563d2e7377559
taurusilver7 <prasad20697@gmail.com>
2021-12-22 22:36:32 +0530

    play atom updated

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -2,8 +2,8 @@ import { atom } from "recoil";
 
 // atom to get the playlist
 export const playlistState = atom({
-  key: "playlistState",
-  default: null,
+  key: "playlistState", // a unique ID w.r.t other atoms/selectors
+  default: null, // default value |(initial value)
 });
 
 // atom to get playlist ID and change the state with ID change.
