53a30a868b4bb6756360010e6b8752b002503c31
taurusilver7 <prasad20697@gmail.com>
2021-12-22 22:39:37 +0530

    atom embedded to song component

--- components/Song.js
+++ components/Song.js
@@ -1,8 +1,12 @@
 import useSpotify from "../hooks/useSpotify";
 import { msToMinsAndSecs } from "../lib/time";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState } from "../atoms/songAtom";
 
 const Song = ({ track, order }) => {
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
   return (
     <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg">
       <div className="flex items-center space-x-4">
