4d6994f5ae84173d2f6d1fdc7b6fcd6c6aaa4c5e
taurusilver7 <prasad20697@gmail.com>
2021-12-24 22:41:22 +0530

    custom hook used in player comp

--- components/Player.js
+++ components/Player.js
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
 
 function Player() {
@@ -9,11 +10,13 @@ function Player() {
   const { data: session, status } = useSession();
   const [isPlaying, setIsPlaying] = useRecoilState(currentTrackIdState);
   const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
   return (
     <div>
       {/* left */}
       <div>
-        <img src="" alt="" />
+        <img src={songInfo?.album.images?.[0]?.url} alt="" />
       </div>
     </div>
   );
