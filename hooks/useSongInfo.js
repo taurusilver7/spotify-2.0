0b95a879e028f1b293e8ee4db0f2a7d3120248a4
taurusilver7 <prasad20697@gmail.com>
2021-12-24 22:39:37 +0530

    fetchSongInfo function

--- hooks/useSongInfo.js
+++ hooks/useSongInfo.js
@@ -10,12 +10,20 @@ const useSongInfo = () => {
   const [songInfo, setSongInfo] = useState(null);
 
   useEffect(() => {
-      const fetchSongInfo = async () => {
-          if (currentTrackId) {
-              const trackInfo = await fetch(`https"//api.spotify.com/v1/tracks/${currentTrackId}`)
+    const fetchSongInfo = async () => {
+      if (currentTrackId) {
+        const trackInfo = await fetch(
+          `https"//api.spotify.com/v1/tracks/${currentTrackId}`,
+          {
+            headers: { Authorization: `Bearer ${spotifyApi.getAccessToken()}` },
           }
+        ).then((res) => res.json());
+        setSongInfo(trackInfo);
       }
+    };
+    fetchSongInfo();
   }, [currentIdTrack, spotifyApi]);
+
   return songInfo;
 };
 
